cfc664561f668605419c871bd40dda722e1cc3e3
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2021-03-22 04:19:00 +0000

    Added audio state triggers base on score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -14,11 +14,54 @@ public class ScoreManager : MonoBehaviour
 
     public void UpdateScore(float scoreToAdd)
     {
-        currentScore += (uint)Mathf.RoundToInt(scoreToAdd*currentMultipler);
+        currentScore += (uint)Mathf.RoundToInt(scoreToAdd * currentMultipler);
         UpdateScoreText();
 
         currentMultipler++;
         UpdateMultiplierText();
+
+        HandleMusicTriggers();
+    }
+
+    private void HandleMusicTriggers()
+    {
+        // Handle Music Triggers
+        int currentState = 0;
+        if (currentState == 0 && currentScore > 5500)
+        {
+            AkSoundEngine.PostEvent("SETSTATE", gameObject);
+            currentState = 1;
+        }
+
+        if (currentState == 1 && currentScore > 6000000)
+        {
+            AkSoundEngine.PostEvent("SETSTATE2", gameObject);
+            currentState = 2;
+        }
+
+        if (currentState == 2 && currentScore > 3000000000)
+        {
+            AkSoundEngine.PostEvent("SETSTATE3", gameObject);
+            currentState = 3;
+        }
+
+        if (currentState == 3 && currentScore > 3000000000000)
+        {
+            AkSoundEngine.PostEvent("SETSTATE4", gameObject);
+            currentState = 4;
+        }
+
+        if (currentState == 4 && currentScore > 400000000000000)
+        {
+            AkSoundEngine.PostEvent("SETSTATE5", gameObject);
+            currentState = 5;
+        }
+
+        if (currentState == 5 && currentScore > 500000000000000000)
+        {
+            AkSoundEngine.PostEvent("SETSTATE6", gameObject);
+            currentState = 6;
+        }
     }
 
     public void DoubleScore()
